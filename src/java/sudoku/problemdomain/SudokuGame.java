b09e8e8733e345ae4176195888816a8ff60ee618
Jordan Felan <felan.jordan94@gmail.com>
2022-09-13 14:49:33 -0500

    adding existing project files to github

---
+++ src/java/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,23 @@
+package sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCOpyOfGridState() {
+        return SudukoUtilities.copyToNewArray(gridState);
+    }
+
+    public SudukoGame(GameState gameState, int[][] gridState){
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+}
