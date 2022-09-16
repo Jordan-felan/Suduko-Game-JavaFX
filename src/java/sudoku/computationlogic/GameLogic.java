bba889afafe60f0668afb23ddca731c201b25573
Jordan Felan <felan.jordan94@gmail.com>
2022-09-15 20:01:04 -0500

    not much time to work today but almost done with game logic

--- src/java/sudoku/computationlogic/GameLogic.java
+++ src/java/sudoku/computationlogic/GameLogic.java
@@ -4,6 +4,10 @@ import sudoku.constants.GameState;
 import sudoku.constants.Rows;
 import sudoku.problemdomain.SudokuGame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static sudoku.problemdomain.SudokuGame.GRID_BOUNDARY;
 
 public class GameLogic {
@@ -28,6 +32,12 @@ public class GameLogic {
         else return false;
     }
 
+    private static boolean columnsAreInvalid(int[][] grid) {
+        for (int xIndex = 0; xIndex <GRID_BOUNDARY; xIndex++){
+            List<Integer> row = new ArrayList<>();
+        }
+    }
+
     private static boolean squaresAreInvalid(int[][] grid) {
         if(rowOfSquaresIsInvalid(Rows.TOP, grid)) return true;
         
@@ -38,7 +48,61 @@ public class GameLogic {
         return false;
     }
 
-    private static boolean rowOfSquaresIsInvalid(Rows top, int[][] grid) {
+    private static boolean rowOfSquaresIsInvalid(Rows value, int[][] grid) {
+        switch (value){
+            case TOP:
+                if(squareIsInvalid(0,0, grid)) return true;
+                if(squareIsInvalid(0,3, grid)) return true;
+                if(squareIsInvalid(0,6, grid)) return true;
+                return false;
+
+            case MIDDLE:
+                if(squareIsInvalid(3,0, grid)) return true;
+                if(squareIsInvalid(3,3, grid)) return true;
+                if(squareIsInvalid(3,6, grid)) return true;
+                return false;
+
+            case BOTTOM:
+                if(squareIsInvalid(6,0, grid)) return true;
+                if(squareIsInvalid(6,3, grid)) return true;
+                if(squareIsInvalid(6,6, grid)) return true;
+                return false;
+
+            default:
+                return false;
+        }
+
+    }
+
+    private static boolean squareIsInvalid(int xIndex, int yIndex, int[][] grid) {
+        int yIndexEnd = yIndex + 3;
+        int xIndexEnd = xIndex + 3;
+
+        List<Integer> square = new ArrayList<>();
+
+        while(yIndex < yIndexEnd){
+            while(xIndex < xIndexEnd){
+                square.add(
+                        grid[xIndex][yIndex]
+                );
+
+                xIndex++;
+            }
+
+            xIndex -= 3;
+
+            yIndex++;
+        }
+        if(collectionHasRepeats(square)) return true;
+        return false;
+
+    }
+
+    private static boolean collectionHasRepeats(List<Integer> collection) {
+        for(int index = 1; index <=GRID_BOUNDARY; index++){
+            if(Collections.frequency(collection, index) > 1) return true;
+        }
+        return false;
     }
 
     private static boolean tilesAreNotFilled(int[][] grid) {
